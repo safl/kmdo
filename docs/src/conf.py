ce5637acd75551d0301a3d3aa2ae8828f9372d8f
Simon A. F. Lund <safl@safl.dk>
2019-04-02 10:57:00 +0200

    docs: fixed sphinx-config

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -5,27 +5,27 @@
 import sys
 import os
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
 extensions = [
-    'sphinx.ext.todo',
-    'sphinx.ext.imgmath',
+    "sphinx.ext.todo",
+    "sphinx.ext.imgmath",
 ]
 
 templates_path = ["_templates"]
-source_suffix = '.rst'
-master_doc = 'index'
+source_suffix = ".rst"
+master_doc = "index"
 
-project = u'kmdo'
-copyright = u'2018, SAFL'
+project = u"kmdo"
+copyright = u"2018, SAFL"
 
-version = '0.0.3'
-release = '0.0.3'
+version = "0.0.3"
+release = "0.0.3"
 
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 if not on_rtd:
     import sphinx_rtd_theme
@@ -33,4 +33,4 @@ if not on_rtd:
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_static_path = ["_static"]
-htmlhelp_basename = 'kmdodoc'
+htmlhelp_basename = "kmdodoc"
